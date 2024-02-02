67d17aaadab37f4e27caeefe9c9205eb1fa52d15
Pavel <Pavel.Bogator@tptlive.ee>
2024-02-02 14:04:19 +0200

    big update

---
+++ Game fun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControlelr : MonoBehaviour
+{
+    public float Speed;
+    public Text ScoreText;
+    public Text WinText;
+    public GameObject Wall;
+    private Rigidbody rb;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Score = 0;
+        SetScoreText();
+        WinText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin")) {
+            other.gameObject.SetActive(false);
+            Score++;
+            SetScoreText();
+
+            if (Score > 3)
+            {
+                Wall.gameObject.SetActive(false);
+            }
+        }
+
+        if (other.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void SetScoreText()
+    {
+        ScoreText.text = "Score " + Score.ToString();
+
+        if (Score >= 5)
+        {
+            WinText.text = "You have won the game! Press R to restart or ESC to quit the game!";
+        }
+    }
+}
